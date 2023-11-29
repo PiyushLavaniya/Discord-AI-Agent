3099fea7c6e9a470d766451350ffccb1ef41fdfe
PiyushLavaniya <1947titan@gmail.com>
2023-11-29 13:26:48 +0530

    Discord Agent

--- bot_utilities/ai_utils.py
+++ bot_utilities/ai_utils.py
@@ -1,36 +1,139 @@
 import aiohttp
 import io
 from datetime import datetime
-import re
-import asyncio
 import time
 import random
-import asyncio
 from urllib.parse import quote
 from bot_utilities.config_loader import load_current_language, config
-from openai import AsyncOpenAI
+import openai
 import os
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
+import json
 
+from langchain.agents import initialize_agent, AgentType, Tool
+from langchain.chains import LLMMathChain
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.prompts import MessagesPlaceholder
+from langchain.schema import SystemMessage
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+from langchain.prompts import PromptTemplate
+from bs4 import BeautifulSoup
+from pydantic import Field
+from langchain.prompts import ChatPromptTemplate
+from langchain.utilities import SerpAPIWrapper
+import requests
+
+os.environ["SERPAPI_API_KEY"] = "41cae65ccd6fde4113d6dc6a08965f583bef5fe4995b5e5ae8a52e56525dd7ce"
+
+load_dotenv(find_dotenv())
+openai.api_key = os.getenv("OPENAI_API_KEY")
 load_dotenv()
 current_language = load_current_language()
 internet_access = config['INTERNET_ACCESS']
 
-openai_client = AsyncOpenAI(
-    api_key = os.getenv('CHIMERA_GPT_KEY'),
-    base_url = "https://api.naga.ac/v1"
+# openai.api_key = os.getenv('CHIMERA_GPT_KEY')
+# openai.api_base = "https://api.naga.ac/v1"
+def sdxl(prompt):
+    response = openai.Image.create(
+    model="sdxl",
+    prompt=prompt,
+    n=1,  # images count
+    size="1024x1024"
 )
+    return response['data'][0]["url"]
 
-async def sdxl(prompt):
-    response = await openai_client.images.generate(
-        model="sdxl",
-        prompt=prompt,
-        n=1,  # images count
-        size="1024x1024"
+
+serpapi= SerpAPIWrapper()
+
+def knowledge_retrieval(query):    
+    # Define the data to be sent in the request
+    data = {
+        "params":{
+            "query":query
+        },
+        "project": "feda14180b9d-4ba2-9b3c-6c721dfe8f63"
+    }
+
+    # Convert Python object to JSON string
+    data_json = json.dumps(data)
+
+    # Send the POST request
+    response = requests.post("https://api-1e3042.stack.tryrelevance.com/latest/studios/6eba417b-f592-49fc-968d-6b63702995e3/trigger_limited", data=data_json)
+    
+    # Check the response status code
+    if response.status_code == 200:
+        return response.json()["output"]["answer"]
+    else:
+        print(f"HTTP request failed with status code {response.status_code}") 
+        
+        
+
+def summary(content):
+    llm = ChatOpenAI(temperature = 0)
+    text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"], chunk_size = 5000, chunk_overlap=100)
+    docs = text_splitter.create_documents([content])
+    map_prompt = """
+    Write a summary of the following text:
+    "{text}"
+    SUMMARY:
+    """
+    map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text"])
+    
+    summary_chain = load_summarize_chain(
+        llm=llm, 
+        chain_type='map_reduce',
+        map_prompt = map_prompt_template,
+        combine_prompt = map_prompt_template,
+        verbose = True
     )
-    return response.data[0].url
 
-async def search(prompt):
+    output = summary_chain.run(input_documents=docs,)
+
+    return output
+
+
+
+def scrape_website(url: str):
+    #scrape website, and also will summarize the content based on objective if the content is too large
+    #objective is the original objective & task that user give to the agent, url is the url of the website to be scraped
+
+    print("Scraping website...")
+    # Define the headers for the request
+    headers = {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+    }
+
+    # Define the data to be sent in the request
+    data = {
+        "url": url        
+    }
+
+    # Convert Python object to JSON string
+    data_json = json.dumps(data)
+
+    # Send the POST request
+    response = requests.post("https://chrome.browserless.io/content?token=0a049e5b-3387-4c51-ab6c-57647d519571", headers=headers, data=data_json)
+    
+    # Check the response status code
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        text = soup.get_text()
+        print("CONTENTTTTTT:", text)
+        if len(text) > 8000:
+            output = summary(text)
+            return output
+        else:
+            return text
+    else:
+        print(f"HTTP request failed with status code {response.status_code}")     
+
+
+
+def search(query):
     """
     Asynchronously searches for a prompt and returns the search results as a blob.
 
@@ -43,64 +146,159 @@ async def search(prompt):
     Raises:
         None
     """
-    if not internet_access or len(prompt) > 200:
-        return
-    search_results_limit = config['MAX_SEARCH_RESULTS']
 
-    if url_match := re.search(r'(https?://\S+)', prompt):
-        search_query = url_match.group(0)
-    else:
-        search_query = prompt
+    url = "https://google.serper.dev/search"
 
-    if search_query is not None and len(search_query) > 200:
-        return
+    payload = json.dumps({
+        "q": query
+    })
+    headers = {
+        'X-API-KEY': 'ab179d0f00ae0bafe47f77e09e62b9f53b3f281d',
+        'Content-Type': 'application/json'
+    }
 
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    blob = f"Search results for: '{search_query}' at {current_time}:\n"
-    if search_query is not None:
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get('https://ddg-api.awam.repl.co/api/search',
-                                       params={'query': search_query, 'maxNumResults': search_results_limit}) as response:
-                    search = await response.json()
-        except aiohttp.ClientError as e:
-            print(f"An error occurred during the search request: {e}")
-            return
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-        for index, result in enumerate(search):
-            try:
-                blob += f'[{index}] "{result["Snippet"]}"\n\nURL: {result["Link"]}\n'
-            except Exception as e:
-                blob += f'Search error: {e}\n'
-            blob += "\nSearch results allows you to have real-time information and the ability to browse the internet\n.As the links were generated by the system rather than the user, please send a response along with the link if necessary.\n"
-        return blob
-    else:
-        blob = "No search query is needed for a response"
-    return blob
+    return response.json()
+
+
+
+def research(query):
+    system_message = SystemMessage(
+        content="""
+        You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
+        you do not make things up, you will try as hard as possible to gather facts & data to back up the research
+            
+        Please make sure you complete the objective above with the following rules:
+        1/ You will always searching for internal knowledge base first to see if there are any relevant information
+        2/ If the internal knowledge doesnt have good result, then you can go search online
+        3/ While search online:
+            a/ You will try to collect as many useful details as possible
+            b/ If there are url of relevant links & articles, you will scrape it to gather more information
+            c/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
+        4/ You should not make things up, you should only write facts & data that you have gathered
+        5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
+        """
+    )
+
+    agent_kwargs = {
+        "system_message": system_message,
+    }
+
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+    #llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
+    tools = [            
+        Tool(
+            name = "search",
+            func = serpapi.run,
+            description = "Useful when you need to answer questions about current events. You should ask targeted questions.",
+        ),          
+        Tool(
+            name = "Scrape_website",
+            func = scrape_website,
+            description = "Use this to load content from a website url"
+        ),   
+    ]
+
+    agent = initialize_agent(
+        tools, 
+        llm, 
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose = True,
+        agent_kwargs=agent_kwargs,
+    )
+
+    results = agent.run(query)
+
+    return results
+    
+    
     
 async def fetch_models():
-    models = await openai_client.models.list()
-    return models
+    return openai.Model.list()
     
-async def generate_response(instructions, search, history):
-    search_results = search if search is not None else "Search feature is disabled"
+agents = {}
+
+def create_agent(id, user_name, ai_name, instructions):
+    system_message = SystemMessage(
+        content=instructions
+    )
+
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+        "system_message": system_message,
+    }
+
+    memory = ConversationBufferWindowMemory(memory_key="memory", return_messages=True, ai_prefix=ai_name, user_prefix=user_name)
+
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+    tools = [                     
+        Tool(
+            name = "research",
+            func = research,
+            description = "Always use this to answer questions about current events, data, or terms that you don't really understand. You should ask targeted questions"
+        ),           
+        Tool(
+            name = "Scrape_website",
+            func = scrape_website,
+            description = "Use this to load content from a website url"
+        ),   
+    ]    
+
+    agent = initialize_agent(
+        tools, 
+        llm, 
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True,
+        agent_kwargs=agent_kwargs,
+        memory=memory
+    )
+
+    agents[id] = agent
+    
+    return agent
+
+
+def generate_response(instructions, user_input):   
+    id = user_input["id"]    
+    message = user_input["message"]
+
+    if id not in agents:
+        user_name = user_input["user_name"]
+        ai_name = user_input["ai_name"]
+        agent = create_agent(id, user_name, ai_name, instructions)
+    else:
+        agent = agents[id]
+    
+    print(message)
+    response = agent.run(message)
+
+    return response
+
+
+def generate_response_old(instructions, search, history):
+    if search is not None:
+        search_results = search
+    elif search is None:
+        search_results = "Search feature is disabled"
     messages = [
             {"role": "system", "name": "instructions", "content": instructions},
             *history,
             {"role": "system", "name": "search_results", "content": search_results},
         ]
-    response = await openai_client.chat.completions.create(
+    response = openai.ChatCompletion.create(
         model=config['GPT_MODEL'],
         messages=messages
     )
     message = response.choices[0].message.content
     return message
 
-async def generate_gpt4_response(prompt):
+
+def generate_gpt4_response(prompt):
     messages = [
             {"role": "system", "name": "admin_user", "content": prompt},
         ]
-    response = await openai_client.chat.chat.completions.create(
+    response = openai.ChatCompletion.create(
         model='gpt-4',
         messages=messages
     )
@@ -112,7 +310,8 @@ async def poly_image_gen(session, prompt):
     image_url = f"https://image.pollinations.ai/prompt/{prompt}?seed={seed}"
     async with session.get(image_url) as response:
         image_data = await response.read()
-        return io.BytesIO(image_data)
+        image_io = io.BytesIO(image_data)
+        return image_io
 
 # async def fetch_image_data(url):
 #     async with aiohttp.ClientSession() as session:
@@ -120,15 +319,15 @@ async def poly_image_gen(session, prompt):
 #             return await response.read()
 
 async def dall_e_gen(model, prompt, size, num_images):
-    response = await openai_client.chat.images.generate(
+    response = openai.Image.create(
         model=model,
         prompt=prompt,
         n=num_images,
         size=size,
     )
     imagefileobjs = []
-    for image in response.data:
-        image_url = image.url
+    for image in response["data"]:
+        image_url = image["url"]
         async with aiohttp.ClientSession() as session:
             async with session.get(image_url) as response:
                 content = await response.content.read()
